18d2b92347d3a34ebd09d243e29e98adbd124756
Jasonquiar <jasonquiar@hotmail.com>
2023-05-08 22:36:31 -0500

    quitamos basura de js.

--- js/main.js
+++ js/main.js
@@ -13,9 +13,3 @@ function removerClaseActivo() {
 	});
 }
 
-const menuIcon = document.querySelector(".nav__item--6");
-const menuList = document.querySelector(".nav__list");
-
-menuIcon.addEventListener("click", () => {
-  menuList.classList.toggle("nav__list--active");
-});
