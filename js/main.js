9c9ded18245e41d0f3d58bf8cf4d187e9525c437
Jasonquiar <jasonquiar@hotmail.com>
2023-05-30 03:58:33 -0500

    Se organiza el sistemas de preguntas y respuestas

--- js/main.js
+++ js/main.js
@@ -1,21 +1,57 @@
+
+
+// const preguntas = document.querySelectorAll(".pregunta_encabezado");
+
+// preguntas.forEach((pregunta) => {
+// 	pregunta.addEventListener("click", () => {
+// 		removerClaseActivo();
+// 		pregunta.nextElementSibling.classList.add("activo");
+// 	});
+// });
+
+// function removerClaseActivo() {
+// 	preguntas.forEach((pregunta) => {
+// 		pregunta.nextElementSibling.classList.remove("activo");
+// 	});
+// }
+
+
+
+
 const preguntas = document.querySelectorAll(".pregunta_encabezado");
 
 preguntas.forEach((pregunta) => {
-	pregunta.addEventListener("click", () => {
-		removerClaseActivo();
-		pregunta.nextElementSibling.classList.add("activo");
-	});
+  pregunta.addEventListener("click", () => {
+    const respuesta = pregunta.nextElementSibling;
+    const arrowIcon = pregunta.querySelector(".arrow-icon");
+
+    if (respuesta.classList.contains("activo")) {
+      respuesta.classList.remove("activo");
+      arrowIcon.classList.remove("rotate");
+    } else {
+      removerClaseActivo();
+      respuesta.classList.add("activo");
+      arrowIcon.classList.add("rotate");
+    }
+  });
 });
 
 function removerClaseActivo() {
-	preguntas.forEach((pregunta) => {
-		pregunta.nextElementSibling.classList.remove("activo");
-	});
+  preguntas.forEach((pregunta) => {
+    const respuesta = pregunta.nextElementSibling;
+    const arrowIcon = pregunta.querySelector(".arrow-icon");
+
+    respuesta.classList.remove("activo");
+    arrowIcon.classList.remove("rotate");
+  });
 }
+
+
+
 
 const enviarCorreoBtn = document.getElementById("enviarCorreo");
 enviarCorreoBtn.addEventListener("click", function() {
-	const asunto = encodeURIComponent("Contacto a Jasonquiar desde su pagina de Github");
+	const asunto = encodeURIComponent("Contacto a Jasonquiar con el fin de...");
 	const destinatario = "jasonquiar@hotmail.com";
 	window.location.href = `mailto:${destinatario}?subject=${asunto}`;
 });
